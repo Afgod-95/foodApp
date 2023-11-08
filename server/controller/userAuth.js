0b158447916056c691d033bee30636cf06eb12cd
Afgod-95 <afgod98@gmail.com>
2023-11-08 15:32:07 +0000

    Hey What the Fuck

--- server/controller/userAuth.js
+++ server/controller/userAuth.js
@@ -5,9 +5,51 @@ const nodemailer = require('nodemailer');
 const emailFormat = new RegExp(/^[a-zA-Z0-9_.+]*[a-zA-Z][a-zAZ0-9_.+]*@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/);
 
 const multer = require('multer');
-const { generateKey } = require('crypto');
 const storage = multer.memoryStorage()
 const upload = multer({storage: storage })
+
+const generateOTP = () => {
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const expirationTime = new Date();
+    expirationTime.setMinutes(expirationTime.getMinutes() + 30); // Expires in 30 minutes
+    console.log("Generated OTP:", otp);
+    console.log("Expiration Time:", expirationTime);
+    return { otp, expirationTime };
+};
+
+
+// Function to send an email with OTP
+const sendVerificationEmail = async (email, otp) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'afgod98@gmail.com',
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+    const mailOptions = {
+        from: 'ShopNeest.com',
+        to: email,
+        subject: 'Account Verification',
+        html: `
+            <p>Hello,</p>
+            <p>Your verification code is: <strong style="font-size: 20px;">${otp}</strong></p>
+            <p>OTP expires in 30 minutes</p>
+            <p>If you did not make this request, please ignore this email, and your password will remain unchanged.</p>
+        `,
+    };
+    return new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(info);
+            }
+        });
+    });
+};
 
 
 //profile pic 
