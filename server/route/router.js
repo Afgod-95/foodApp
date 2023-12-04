990b9db4b3f6383d224238e6a47abfee5c89a57b
Afgod-95 <afgod98@gmail.com>
2023-12-04 12:24:53 +0000

    Hey

--- server/route/router.js
+++ server/route/router.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { OAuth2Client } = require('google-auth-library');
+const mongoose = require('mongoose');
+const User = require('../models/googleModel.js')
 
 const roles = {
     user: 'user',
@@ -53,5 +58,56 @@ router.post('/api/postproducts', postProducts)
 router.put('/api/products/id', updateSingleProduct)
 router.delete('/api/products/id', deleteSingleProduct)
 
+
+
+// Configure passport and Google OAuth strategy
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'https://restaurantapi-bsc7.onrender.com/auth/google/callback',
+  userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+  passReqToCallback: true,
+}));
+
+// Define route for Google login
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// Callback route after Google authentication
+router.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/Main',
+  failureRedirect: '/sign-up',
+}));
+
+// Route to handle user information after login
+router.get('/', (req, res) => {
+  if (!req.user) {
+    return res.redirect('/login');
+  }
+  const { displayName, email } = req.user.profile;
+  res.send(`Welcome, ${displayName}! Your email is ${email}`);
+});
+
+// Signup logic (example using MongoDB)
+
+
+
+router.post('/signup', async (req, res) => {
+    const { name, email, googleId } = req.body;
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        // Already signed up
+        return res.status(400).send({ message: 'User already exists' });
+      }
+  
+      const newUser = await User.create({ name, email, googleId });
+      // Send success response or redirect to login
+      res.status(200).send({ message: 'User successfully signed up' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: 'Error signing up user' });
+    }
+  });
+  
 
 module.exports = router
